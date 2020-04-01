daa6b23deb75ef297be9c50cdbf1997a41ccf347
vtr <vrigolon@gmail.com>
2020-04-01 14:11:14 +0100

    add instructor filter

--- src/app/controllers/instructors.js
+++ src/app/controllers/instructors.js
@@ -5,10 +5,21 @@ const { age, date } = require('../../lib/utils')
 module.exports = {
   index(req, res) {
 
-    Instructor.all(function(instructors) {
-      return res.render("instructors/index", { instructors })
+    const { filter } = req.query
 
-    })
+    if(filter) {
+
+      Instructor.findBy(filter, function(instructors) {
+        return res.render("instructors/index", { instructors })
+
+      })
+
+    }else {
+      Instructor.all(function(instructors) {
+        return res.render("instructors/index", { instructors })
+
+      })
+    }
     
     
 
--- src/app/models/Member.js
+++ src/app/models/Member.js
@@ -45,7 +45,11 @@ module.exports = {
     })
   },
   find(id, callback) {
-    db.query(`SELECT * FROM members WHERE id = $1`, [id], function(err, results) {
+    db.query(`
+    SELECT members.*, instructors.name AS instructor_name 
+    FROM members
+    LEFT JOIN instructors ON (members.instructor_id = instructors.id)
+    WHERE members.id = $1`, [id], function(err, results) {
       if(err) throw `"Database Error!" ${err}`
       callback(results.rows[0])
     })
